0e06afe0728556df4381f222ab6b004c65acd107
kennny <kennyli699@gmail.com>
2022-08-03 15:46:58 -0700

    complete

--- README.md
+++ README.md
@@ -22,16 +22,16 @@ Note that Python 3.10 should automatically have pip installed with it. If it isn
 ## Basic Usage
 This script scrapes data from WalkScore's website. 
 
-By default, the script opens 'sample.csv', then gets the address from column 'Match_addr'. You can edit main.py to change the names.
+By default, the script opens 'input.csv', then gets the address from column 'Match_addr'. You can edit main.py to change the names.
 
 The script grabs the following score types: walk, bike, transit. It then creates a timestamped folder of when the script was run with graphs for each address.
-It will then write the scores in a timestamped copy of the original csv file but with the scores.
+It will then write the scores in a timestamped copy of the original csv file but with the scores added.
 
-
+Note: Some scores may have 0 values in one of the score categories.
 # Important
-Due to the absence of a WalkScore API and rate limits, the script cannot return all csv lines in one go. I have tested that ~50/60% of the csv file will run, and the remainder will be placed in the recheck.csv file. You can then copy this to put it back in the sample.csv file to recheck. This is due to the fact that the script cannot infinitely loop until the asyncronous timer ends.
+Scraping WalkScore should not have a large ratelimit. However, roughly <1% of the csv file will be skipped. The 'recheck.csv' file has all error generated csv lines (wrong address formatting, timeouts). You can simply copy and paste it back in input.csv to recheck them.
 
-
+Addresses with no scores will always be put in the recheck.csv. Essentially, the ones that repeatedly have errors should not be used for data analysis.
 
 ![App Screenshot](https://i.ibb.co/XZrBkZG/new.jpg)
 ![App Screenshot](https://i.ibb.co/3zvYpGR/ewven-newer.jpg)
--- main.py
+++ main.py
@@ -5,21 +5,25 @@ import asyncio
 import datetime
 from draw import Graph
 import os
+import platform
 
-##edit below to change csv name
-sampling = ['Match_addr', 'x', 'IN_City', 'score', 'IN_Address', 'IN_Postal', 'Subregion', 'y', 'Postal', 'PostalExt',
-            'ResultID', 'City']
 
+# edit below to change csv name
 file = open('input.csv', encoding='utf8')
 outside = []
+completed = []
 current_line = ''
 sample = csv.DictReader(file)
+sampling = sample.fieldnames
+hard_sampling = list(sampling)
+hard_sampling.append('walk')
+hard_sampling.append('bike')
+hard_sampling.append('transit')
 now = datetime.datetime.now()
 current_directory = os.getcwd()
-final_directory = os.path.join(current_directory, str(now))
+final_directory = os.path.join(os.sep, current_directory + os.sep, str(now))
 os.mkdir(final_directory)
 os.chdir(final_directory)
-
 
 async def main_scrape():
     async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
@@ -33,22 +37,47 @@ async def main_scrape():
 
 
 async def get_scores(session, address, full):
+    global outside
+    global completed
+    bully = full
     url = f'https://www.walkscore.com/score/{address}'
-    async with session.get(url) as get:
-        response = await get.text()
-        try:
-            walk = int(re.search(pattern=r'has a Walk Score of (\d*)', string=response).group(1))
-            bike = int(re.search(pattern=r'png" alt="(\d*) Bike Score of', string=response).group(1))
-            transit = int(re.search(pattern=r'png" alt="(\d*) Transit Score of', string=response).group(1))
+    try:
+        async with session.get(url) as get:
+            response = await get.text()
+            try:
+                walk = int(re.search(pattern=r'has a Walk Score of (\d*)', string=response).group(1))
+            except:
+                walk = 0
+            try:
+                bike = int(re.search(pattern=r'png" alt="(\d*) Bike Score of', string=response).group(1))
+            except:
+                bike = 0
+            try:
+                transit = int(re.search(pattern=r'png" alt="(\d*) Transit Score of', string=response).group(1))
+            except:
+                transit = 0
             print(f'{address} walk: {walk} bike: {bike} transit: {transit}')
             g = Graph(walk, bike, transit, address)
             g.construct()
-        except AttributeError:
-            global outside
-            outside.append(full)
+            if walk == 0 and bike == 0 and transit == 0:
+                outside.append(full)
+            else:
+                bully['walk'] = walk
+                bully['bike'] = bike
+                bully['transit'] = transit
+                completed.append(bully)
+    except aiohttp.ServerDisconnectedError:
+        outside.append(full)
 
 
-asyncio.run(main_scrape())
+try:
+    asyncio.run(main_scrape())
+except asyncio.exceptions.TimeoutError:
+    pass
+with open(f'{now}.csv', 'w', newline='') as completed_scores:
+    dict_writer = csv.DictWriter(completed_scores, hard_sampling)
+    dict_writer.writeheader()
+    dict_writer.writerows(completed)
 os.chdir(current_directory)
 with open('recheck.csv', 'w', newline='') as output_file:
     dict_writer = csv.DictWriter(output_file, sampling)
