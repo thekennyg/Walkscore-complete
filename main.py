b37a750c3d4a7edbb21de6855890245450ef0493
kennny <kennyli699@gmail.com>
2022-08-02 22:57:39 -0700

    complete

--- main.py
+++ main.py
@@ -2,24 +2,30 @@ import re
 import csv
 import aiohttp
 import asyncio
-from csv import writer
 import datetime
 from draw import Graph
 import os
+
 ##edit below to change csv name
-file = open('sample.csv', encoding='utf8')
-exit = []
+sampling = ['Match_addr', 'x', 'IN_City', 'score', 'IN_Address', 'IN_Postal', 'Subregion', 'y', 'Postal', 'PostalExt',
+            'ResultID', 'City']
+
+file = open('input.csv', encoding='utf8')
+outside = []
+current_line = ''
 sample = csv.DictReader(file)
 now = datetime.datetime.now()
 current_directory = os.getcwd()
 final_directory = os.path.join(current_directory, str(now))
 os.mkdir(final_directory)
 os.chdir(final_directory)
+
+
 async def main_scrape():
     async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
         tasks = []
         for line in sample:
-            #edit below to change csv column name
+            # edit below to change csv column name
             addy = line['Match_addr']
             future = asyncio.ensure_future(get_scores(session, addy, line))
             tasks.append(future)
@@ -27,7 +33,6 @@ async def main_scrape():
 
 
 async def get_scores(session, address, full):
-    global exit
     url = f'https://www.walkscore.com/score/{address}'
     async with session.get(url) as get:
         response = await get.text()
@@ -38,19 +43,16 @@ async def get_scores(session, address, full):
             print(f'{address} walk: {walk} bike: {bike} transit: {transit}')
             g = Graph(walk, bike, transit, address)
             g.construct()
-        except:
-            exit.append(full)
-
-def check_exit():
-    with open('modify.txt', mode='a') as x:
-        for i in exit:
-            x.write(str(i))
-
-
-
+        except AttributeError:
+            global outside
+            outside.append(full)
 
 
 asyncio.run(main_scrape())
-check_exit()
+os.chdir(current_directory)
+with open('recheck.csv', 'w', newline='') as output_file:
+    dict_writer = csv.DictWriter(output_file, sampling)
+    dict_writer.writeheader()
+    dict_writer.writerows(outside)
 
 # https://compiletoi.net/fast-scraping-in-python-with-asyncio/
